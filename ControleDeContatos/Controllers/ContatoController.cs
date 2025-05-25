7be8c467f12136865289f03ce5240f1bfa76e5c5
crist <crist@DESKTOP-TV121RO>
2025-05-25 16:11:33 -0300

    Add project files.

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,64 @@
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    
+    public class ContatoController : Controller
+    {
+        private readonly IContatoRepositorio _contatoRepositorio;
+        public ContatoController(IContatoRepositorio contatoRepositorio)
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
+        public IActionResult Index()
+        {
+            List<ContatoModel> contatos = _contatoRepositorio.BuscarTodos();
+            return View(contatos);
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar(int id)
+        {
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+        public IActionResult Apagar(int id)
+        {
+            _contatoRepositorio.Apagar(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Adicionar(contato);
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+
+                }
+
+                return View(contato);
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = "Ops, não conseguimos cadastrar o seu contato, tente novamente, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+            _contatoRepositorio.Atualizar(contato);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ControleDeContatos/Data/BancoContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContextFactory : IDesignTimeDbContextFactory<BancoContext>
+    {
+        public BancoContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // pega o appsettings.json
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<BancoContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DataBase"));
+
+            return new BancoContext(optionsBuilder.Options);
+        }
+    }
+}
